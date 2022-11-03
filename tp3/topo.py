71e5c8fc171a4932775182a5012fb23982d0dd23
osadio <osadio@outlook.com>
2022-11-03 22:31:41 +0000

    Create topo.py

---
+++ tp3/topo.py
@@ -0,0 +1,28 @@
+class MyTopo( Topo ):  
+    def __init__( self ):
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add Switches
+        s1 = self.addSwitch("s1")
+        s2 = self.addSwitch("s2")
+        s3 = self.addSwitch("s3")
+        s4 = self.addSwitch("s4")
+        s5 = self.addSwitch("s5")
+
+        # Add Hosts
+        h1 = self.addHost("h1")
+        h2 = self.addHost("h2")
+
+        # Add links Host to Switch
+        self.addLink(h1, s1)
+        self.addLink(s2, s5)
+
+        # Add links Switch to Swttch
+        self.addLink(s1, s2)
+        self.addLink(s1, s3)
+        self.addLink(s2, s5)
+        self.addLink(s3, s4)
+        self.addLink(s4, s5)
+
+topos = { "mytopo": ( lambda: MyTopo() ) }
