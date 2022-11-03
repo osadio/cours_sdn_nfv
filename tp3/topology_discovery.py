eea9a72b1f36c85acbfa418088679e9958341a89
osadio <osadio@outlook.com>
2022-11-03 23:47:11 +0000

    Create topology_discovery.py

---
+++ tp3/topology_discovery.py
@@ -0,0 +1,12 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+
+from ryu.topology import event, switches
+from ryu.topology.api import get_switch, get_link
+
