17dba89c768dd31004699a0caaa9364e6b4b0ce0
osadio <osadio@outlook.com>
2022-11-04 00:11:53 +0000

    Update topology_discovery.py

--- tp3/topology_discovery.py
+++ tp3/topology_discovery.py
@@ -8,5 +8,27 @@ from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 
 from ryu.topology import event, switches
-from ryu.topology.api import get_switch, get_link
+from ryu.topology.api import get_switch, get_link, get_host
 
+class TopologyDiscovery(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    
+    # List the event list should be listened.
+	  events = [event.EventSwitchEnter, event.EventSwitchLeave, event.EventPortAdd,
+              event.EventPortDelete, event.EventPortModify, event.EventLinkAdd, 
+              event.EventLinkDelete]
+
+    def __init__(self, *args, **kwargs):
+        super(TopologyDiscovery, self).__init__(*args, **kwargs)
+        self.topology_api_app = self
+        
+    @set_ev_cls(events)
+    def get_topology(self, ev):
+        switch_list = get_switch(self.topology_api_app, None)
+        links_list = get_link(self.topology_api_app, None)
+        
+        print(switch_list)
+        print(links_list)
+        
+
+   
