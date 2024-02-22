bbec822b6a58d1df50756f18e2c1277e557e1cb1
osadio <osadio@outlook.com>
2024-02-22 22:49:38 +0000

    Update ring_topology.py

--- tp1/ring_topology.py
+++ tp1/ring_topology.py
@@ -1,1 +1,27 @@
+from mininet.topo import Topo
 
+class MyTopo( Topo ):  
+     def build( self ):     
+        # Add Switches
+        s1 = self.addSwitch("s1", stp=True)
+        s2 = self.addSwitch("s2", stp=True)
+        s3 = self.addSwitch("s3", stp=True)
+        s4 = self.addSwitch("s4", stp=True)
+
+        # Add Hosts
+        h1 = self.addHost("h1", ip="10.0.0.1")
+        h2 = self.addHost("h2", ip="10.0.0.2")
+        h3 = self.addHost("h3", ip="10.0.0.3")
+
+        # Add links Host to Switch
+        self.addLink(h1, s1, cls=TCLink, bw=1000, delay=1)
+        self.addLink(h2, s2, cls=TCLink, bw=1000, delay=1)
+        self.addLink(h3, s3, cls=TCLink, bw=1000, delay=1)
+
+        # Add links Switch to Swttch
+        self.addLink(s1, s2, cls=TCLink, bw=10000, delay=10)
+        self.addLink(s2, s3, cls=TCLink, bw=100, delay=100)
+        self.addLink(s3, s4, cls=TCLink, bw=1000, delay=50)
+        self.addLink(s4, s1, , cls=TCLink, bw=10000, delay=10)
+
+topos = { "mytopo": ( lambda: MyTopo() ) }
