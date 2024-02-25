acb419325628a1d7b1592486b8e0ffee3f664efb
osadio <osadio@outlook.com>
2024-02-25 01:31:44 +0000

    Update simple_topology.py

--- tp2/simple_topology.py
+++ tp2/simple_topology.py
@@ -1,1 +1,16 @@
+from mininet.topo import Topo
 
+class MyTopo( Topo ):  
+     def build( self ):
+        # Add Switches
+        s1 = self.addSwitch("s1")
+
+        # Add Hosts
+        h1 = self.addHost("h1")
+        h2 = self.addHost("h2")
+
+        # Add links Host to Switch
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+
+topos = { "mytopo": ( lambda: MyTopo() ) }
